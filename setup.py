95a56711688fa0ef3b0fac446154604c5f105e59
theTarkus <eminencefronts@gmail.com>
2016-07-19 19:11:33 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from setuptools import setup, find_packages
+
+setup(
+	name="django-semanticui-forms",
+	version=str("1"),
+	description="django-semanticui-forms",
+	author="Michael Tark",
+	author_email="theTarkus@users.noreply.github.com",
+	url="https://github.com/theTarkus/django-semantic-ui-forms",
+	install_requires=["django>=1.8"],
+	packages=find_packages()
+)
