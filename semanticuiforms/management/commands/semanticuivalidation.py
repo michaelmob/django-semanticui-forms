7f37e67e5c56508ab7be69f3bbfc6d18006224ae
thetarkus <michael@localhost.localdomain>
2017-05-05 03:24:50 -0400

    1.6.3 Basic form validation config generator

---
+++ semanticuiforms/management/commands/semanticuivalidation.py
@@ -0,0 +1,76 @@
+import importlib
+import json
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+	help = "Closes the specified poll for voting"
+
+	beginning_wrap = "$(\".ui.%s.form\").form({\n\tfields:"
+	ending_wrap = "});"
+
+
+	def add_arguments(self, parser):
+		parser.add_argument("form", nargs="+", type=str, help="Absolute path of form class")
+		parser.add_argument("--shorthand", action="store_true", help="Use shorthand version")
+
+
+	def handle(self, *args, **options):
+		path = str("".join(options["form"])).split(".")
+		klass = getattr(importlib.import_module(".".join(path[:-1])), path[-1])
+
+		form = klass()
+		fields = {}
+		
+		# Loop over each field and create a base and then add rules to it
+		for key, field in form.fields.items():
+			fields[key] = {
+				"identifier": key,
+				"rules": []
+			}
+
+			# Required
+			if field.required:
+				fields[key]["rules"].append({
+					"type": "checked" if type(field).__name__ == "BooleanField" else "empty",
+					"prompt": str(field.error_messages["required"]),
+				})
+
+			# Max Length
+			if field.error_messages.get("max_length") and getattr(field, "max_length"):
+				fields[key]["rules"].append({
+					"type": "maxLength[%s]" % getattr(field, "max_length"),
+					"prompt": str(field.error_messages["max_length"]),
+				})
+
+			# Min Length
+			if field.error_messages.get("min_length") and getattr(field, "min_length"):
+				fields[key]["rules"].append({
+					"type": "minLength[%s]" % getattr(field, "min_length"),
+					"prompt": str(field.error_messages["min_length"]),
+				})
+
+		# Create shorthand field
+		if options.get("shorthand"):
+			shorthand_fields = {}
+
+			for key, value in fields.items():
+				for rule in value["rules"]:
+					if not shorthand_fields.get(key):
+						shorthand_fields[key] = []
+					shorthand_fields[key].append(rule["type"])
+
+				# 
+				if len(shorthand_fields[key]) < 2:
+					shorthand_fields[key] = shorthand_fields[key][0]
+
+			fields = shorthand_fields
+
+		# Data
+		data = json.dumps(fields, sort_keys=True, indent="\t", separators=(',', ': '))
+
+		# Output
+		self.stdout.write(self.beginning_wrap % form.__class__.__name__.lower())
+		for line in data.splitlines():
+			self.stdout.write("\t" + line);
+		self.stdout.write(self.ending_wrap)
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name="django-semanticui-forms",
-	version=str("1.6.2"),
+	version=str("1.6.3"),
 	description="Effortlessly style all of your Django forms and form fields with Semantic UI wrappers.",
 	author="Michael",
 	author_email="thetarkus@users.noreply.github.com",
