ba2a2baa08a19fafde154eb741b0b26bb3492707
thetarkus <michaelmob@protonmail.com>
2017-12-16 13:53:01 -0500

    1.6.5
    Include __init__.py in management/commands
    Include static files in MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-# python setup.py sdist upload -r pypi
+"""
+sudo apt install python3-pip
+sudo pip3 install twine
+
+python3 setup.py sdist
+twine upload dist/django-semanticui-forms-1.6.5.tar.gz
+"""
 from setuptools import setup, find_packages
 
 setup(
 	name="django-semanticui-forms",
-	version=str("1.6.4"),
+	version=str("1.6.5"),
 	description="Effortlessly style all of your Django forms and form fields with Semantic UI wrappers.",
 	author="Michael",
 	author_email="thetarkus@users.noreply.github.com",
